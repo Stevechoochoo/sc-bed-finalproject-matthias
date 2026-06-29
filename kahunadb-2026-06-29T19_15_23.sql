7e4d71dcbe5104ebb1b1b473f60b6d05a7bbd41e
Matthias Schembri <matthiasschembri87@gmail.com>
2026-06-29 19:17:32 +0200

    Created database and tables for kahuna

---
+++ kahunadb-2026-06-29T19_15_23.sql
@@ -0,0 +1,55 @@
+
+CREATE TABLE Products
+(
+  product_id    INT          NOT NULL AUTO_INCREMENT,
+  serial_number VARCHAR(50)  NOT NULL,
+  product_name  VARCHAR(255) NOT NULL,
+  warranty      INT          NOT NULL,
+  PRIMARY KEY (product_id)
+);
+
+ALTER TABLE Products
+  ADD CONSTRAINT UQ_product_id UNIQUE (product_id);
+
+ALTER TABLE Products
+  ADD CONSTRAINT UQ_serial_number UNIQUE (serial_number);
+
+CREATE TABLE RegisteredProducts
+(
+  rp_id         INT  NOT NULL AUTO_INCREMENT,
+  user_id       INT  NOT NULL,
+  product_id    INT  NOT NULL,
+  purchase_date DATE NOT NULL,
+  PRIMARY KEY (rp_id)
+);
+
+ALTER TABLE RegisteredProducts
+  ADD CONSTRAINT UQ_rp_id UNIQUE (rp_id);
+
+CREATE TABLE Users
+(
+  user_id  INT                    NOT NULL AUTO_INCREMENT,
+  name     VARCHAR(100)           NOT NULL,
+  surname  VARCHAR(100)           NOT NULL,
+  email    VARCHAR(255)           NOT NULL,
+  password VARCHAR(255)           NOT NULL,
+  role     ENUM('client','admin') NOT NULL DEFAULT 'client',
+  token    VARCHAR(255)           NULL    ,
+  PRIMARY KEY (user_id)
+);
+
+ALTER TABLE Users
+  ADD CONSTRAINT UQ_user_id UNIQUE (user_id);
+
+ALTER TABLE Users
+  ADD CONSTRAINT UQ_email UNIQUE (email);
+
+ALTER TABLE RegisteredProducts
+  ADD CONSTRAINT FK_Users_TO_RegisteredProducts
+    FOREIGN KEY (user_id)
+    REFERENCES Users (user_id);
+
+ALTER TABLE RegisteredProducts
+  ADD CONSTRAINT FK_Products_TO_RegisteredProducts
+    FOREIGN KEY (product_id)
+    REFERENCES Products (product_id);
