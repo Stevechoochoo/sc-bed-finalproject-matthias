ac4bbb0eef0e93727c7e2161ace3e037a18f3edf
Matthias Schembri <matthiasschembri87@gmail.com>
2026-06-29 19:23:28 +0200

    Inserted data for Products table

--- db/db.sql
+++ db/db.sql
@@ -2,6 +2,9 @@ CREATE DATABASE IF NOT EXISTS kahuna;
 
 USE kahuna;
 
+
+-- Tables Creation
+
 CREATE TABLE Products
 (
   product_id    INT          NOT NULL AUTO_INCREMENT,
@@ -56,3 +59,17 @@ ALTER TABLE RegisteredProducts
   ADD CONSTRAINT FK_Products_TO_RegisteredProducts
     FOREIGN KEY (product_id)
     REFERENCES Products (product_id);
+
+-- Product Table data insertion
+
+INSERT INTO Products (serial_number, product_name, warranty) VALUES
+('KHWM8199911', 'CombiSpin Washing Machine', 2),
+('KHWM8199912', 'CombiSpin + Dry Washing Machine', 2),
+('KHMW789991', 'CombiGrill Microwave', 1),
+('KHWP890001', 'K5 Water Pump', 5),
+('KHWP890002', 'K5 Heated Water Pump', 5),
+('KHSS988881', 'Smart Switch Lite', 2),
+('KHSS988882', 'Smart Switch Pro', 2),
+('KHSS988883', 'Smart Switch Pro V2', 2),
+('KHHM89762', 'Smart Heated Mug', 1),
+('KHSB0001', 'Smart Bulb 001', 1);
